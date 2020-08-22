a217f20f41e5c9ad0e9644b1582a85c7af80563f
Sushant Babu <47374808+SushantBabu97@users.noreply.github.com>
2020-08-22 19:24:17 +0545

    Update Find_The_Runner_Up_Score.py

--- Find_The_Runner_Up_Score.py
+++ Find_The_Runner_Up_Score.py
@@ -1,6 +1,6 @@
 """
 Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
-
+ 
 For list [2,3,6,6,5] print 5 as it is the second largest score.
 """
 
