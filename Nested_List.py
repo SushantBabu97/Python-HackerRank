2a946a996455d1bb50eea614a7f8ffb1a50d0817
Sushant Babu <47374808+SushantBabu97@users.noreply.github.com>
2020-08-22 19:24:48 +0545

    Update Nested_List.py

--- Nested_List.py
+++ Nested_List.py
@@ -3,7 +3,7 @@
 """
 Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
 
-Example:: 
+Example:: ::
 
 records=[["chi",20.0],["beta",50.0],["alpha",50.0]]
 
